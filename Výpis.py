0c7d22dd0d38e18ee083f0ca084d6b2b8d739619
Nikča <hu3op111@gmail.com>
2025-02-11 15:22:46 +0100

    druha hodina

---
+++ Výpis.py
@@ -0,0 +1,8 @@
+jmeno = "Nikola"
+prijmeni = "Mašková"
+vek = 15
+vyska = 1.65
+pohlavi = 1
+print(jmeno, prijmeni,)
+print("Je jí", vek ,"let a meří", vyska, "metrů.")
+print("Pohlaví - ", pohlavi)
