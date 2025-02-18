3f14a5e54665b8d882f367fe93106ffb7d555679
Nikča <hu3op111@gmail.com>
2025-02-18 15:04:01 +0100

    pivo

---
+++ input.py
@@ -0,0 +1,22 @@
+#aha takze alkoholismus, docela se citim jako ořech, nevim co mam delat
+#v patek bych docela chtela.... spagety? mozna jo. hodily by se. nebo mozna zase kure? ani nevim. mam hlad. treba jenom cerealie. uz dlouho jsem je nemela
+#proooocccccc nam musi zamykat kuchyn :(( chci si jenom jit pro horkou vodu... :P
+sklenice = "mléko"
+sklenicka = "pivson"
+sklenka = " "
+#ted kdybych aspon umela programovat. ja to NEUMIM. matou me ty cisla, proc nemuzou byt proste jiny nazvy? chci barvy
+#dneska uz nemam co jinyho jist.
+print(sklenice, sklenicka, sklenka)
+print("Právě jsme přelili sklenici do sklenky")
+sklenka = sklenice
+print(sklenice, sklenicka, sklenka)
+#wow, ted aby to fungovalo
+print("Teď přelijeme skleničku do sklenice")
+sklenice = sklenicka
+print(sklenice, sklenicka, sklenka)
+#absolutne nechapu pointu tohoto ale to nevadi. do skoly nechodim od toho, abych se ptala proc neco delam.
+print("Přelíváme sklenku do skleničky")
+sklenice = sklenka
+sklenka = " "
+print(sklenice, sklenicka, sklenka)
+#KURVA NEMAM TO OPRAVENY. nevadi... opravim to doma
